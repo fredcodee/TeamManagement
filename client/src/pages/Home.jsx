9fd72d7d6a35f26e950bedbaf00b0ab425c1b3be
fredcodee <fredcode23@gmail.com>
2023-07-26 16:10:32 +0800

    done with section 4&5

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import logo from "../assets/images/teamlogo.png"
 import "../assets/styles/homepage.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faWandMagicSparkles, faCode, faPeopleGroup, faMagnifyingGlassChart, faDiagramProject, faListCheck} from '@fortawesome/free-solid-svg-icons'
+import { faWandMagicSparkles, faCode, faPeopleGroup, faMagnifyingGlassChart, faDiagramProject, faListCheck } from '@fortawesome/free-solid-svg-icons'
 
 const Home = () => {
     return (
@@ -58,7 +58,7 @@ const Home = () => {
                         </div>
                         <div className='options-list'>
                             <div className='section2_icons'>
-                            <FontAwesomeIcon icon={faCode} style={{color: "#48c767",}} />
+                                <FontAwesomeIcon icon={faCode} style={{ color: "#48c767", }} />
                             </div>
                             <div>
                                 Software Development
@@ -66,7 +66,7 @@ const Home = () => {
                         </div>
                         <div className='options-list'>
                             <div className='section2_icons'>
-                            <FontAwesomeIcon icon={faMagnifyingGlassChart} style={{color: "#3da5f5",}} />
+                                <FontAwesomeIcon icon={faMagnifyingGlassChart} style={{ color: "#3da5f5", }} />
                             </div>
                             <div>
                                 Marketing
@@ -74,7 +74,7 @@ const Home = () => {
                         </div>
                         <div className='options-list'>
                             <div className='section2_icons'>
-                            <FontAwesomeIcon icon={faDiagramProject} style={{color: "#ffc038",}} />
+                                <FontAwesomeIcon icon={faDiagramProject} style={{ color: "#ffc038", }} />
                             </div>
                             <div>
                                 Project Management
@@ -82,7 +82,7 @@ const Home = () => {
                         </div>
                         <div className='options-list'>
                             <div className='section2_icons'>
-                            <FontAwesomeIcon icon={faListCheck} style={{color: "#ff5e4d",}} />
+                                <FontAwesomeIcon icon={faListCheck} style={{ color: "#ff5e4d", }} />
                             </div>
                             <div>
                                 Task Management
@@ -90,8 +90,8 @@ const Home = () => {
                         </div>
                         <div className='options-list'>
                             <div className='section2_icons'>
-                        <FontAwesomeIcon icon={faPeopleGroup} style={{color: "#ffb347",}} />
-                        </div>
+                                <FontAwesomeIcon icon={faPeopleGroup} style={{ color: "#ffb347", }} />
+                            </div>
                             <div>
                                 Team Operations
                             </div></div>
@@ -99,8 +99,44 @@ const Home = () => {
 
                 </div>
                 <div className='p-11 text-center'>
-                    <a href='#' type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get Started</a>
+                    <a href='#' type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-lg px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get Started</a>
                     <p className='text-sm'>No credit card needed or payment   ✦   Unlimited time / Free plan</p>
+                </div>
+            </div>
+            <div className='section3'>
+                <img src="https://dapulse-res.cloudinary.com/image/upload/f_auto,q_auto/remote_mondaycom_static/uploads/NaamaGros/HP_tests/HP_asset_white_bg.png" alt="" />
+            </div>
+            <div className='container mx-auto section4'>
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                    <div className='h-auto max-w-full '>
+                        <h2 className='font-bold text-3xl'>The Work OS that lets you
+                            shape workflows, <br /> your way</h2>
+                    </div>
+                    <div className='h-auto max-w-full '>
+                        <p className='text-lg pb-2'>Boost your team’s alignment, efficiency, and productivity <br /> by customizing any workflow to fit your needs.</p>
+                        <a href='#' type="button" class="text-white bg-pink-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-lg px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get Started</a>
+                    </div>
+                </div>
+            </div>
+            <div className='section5 p-5'>
+                <div className='container mx-auto'>
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                        <div className='h-auto max-w-full '>
+                            <h2 className='font-bold text-3xl'>Streamline your work for maximum productivity</h2>
+                            <p className='text-lg pt-4'>Centralize all your work, processes, tools, and files into one Work OS. Connect teams, bridge silos, and maintain one source of truth across your organization.</p>
+                        </div>
+                        <div className='h-auto max-w-full '>
+                            <h2 className='font-bold text-3xl'>Bring teams together to
+                                drive business impact</h2>
+                            <p className='text-lg pt-4'>Collaborate effectively organization-wide to get a clear picture of all your work. Stay in the loop with easy-to-use automations and real-time notifications.</p>
+                        </div>
+                    </div>
+                    <div className='h-auto max-w-full pt-11'>
+                            <h2 className='font-bold text-3xl'>Stay on track to
+                                reach your goals, faster</h2>
+                            <p className='text-lg p-4'>Get a high-level overview of your organization with customizable dashboards. Make confident decisions and easily scale workflows for your evolving needs.</p>
+                            <a href='#' type="button" class="text-white bg-blue-900 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-lg px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get Started</a>
+                        </div>
                 </div>
             </div>
         </div>
