4a664d8f1b9e339473d3cf1c2bce366fbdb51674
fredcodee <fredcode23@gmail.com>
2023-08-03 14:51:59 +0800

    added search projects

--- client/src/pages/WorkSpace.jsx
+++ client/src/pages/WorkSpace.jsx
@@ -14,6 +14,8 @@ const WorkSpace = () => {
     const [user, setUser] = useState([]);
     const history = useNavigate();
     const [projects, setProjects] = useState([]);
+    const [projectcopy, setProjectcopy] = useState([]);
+    const [search, setSearch] = useState('');
     const [error, setError] = useState(null);
 
     useEffect(() => {
@@ -45,9 +47,23 @@ const WorkSpace = () => {
 
             const data = await response.data;
             setProjects(data);
+            setProjectcopy(data);
+            console.log(data);
         }
         catch(error){
             setError(error.response.data.message);
+        }
+    }
+
+    const handleSearch=(e) => {
+        try {
+            e.preventDefault();
+            const filteredProjects = projectcopy.filter((project) => 
+                project.name.includes(search)
+            );
+            setProjects(filteredProjects);
+        } catch (error) {
+            console.log(error);
         }
     }
 
@@ -105,10 +121,10 @@ const WorkSpace = () => {
                                 <a href="#">Your Projects</a>
                             </div>
                             <div>
-                                <form className="flex items-center">
+                                <form className="flex items-center" onSubmit={handleSearch}>
                                     <label htmlFor="simple-search" className="sr-only">Search</label>
                                     <div className="relative w-full">
-                                        <input type="text" id="simple-search" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Projects ..." required />
+                                        <input type="text" id="simple-search" onChange={e => setSearch(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Projects ..." required />
                                     </div>
                                     <button type="submit" className="p-2.5 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                         <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
