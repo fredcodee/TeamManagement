99e1cacd62bdc65574abcabc44921f6213aec6ce
fredcodee <fredcode23@gmail.com>
2023-08-10 18:47:07 +0800

    remove user from team

--- client/src/pages/UserManagement.jsx
+++ client/src/pages/UserManagement.jsx
@@ -61,7 +61,7 @@ const UserManagement = () => {
         setIsOpen2(!isOpen2)
     }
 
-    // for remove user
+    // for remove user fron team
     const togglePopup3 = () => {
         setIsOpen3(!isOpen3);
     }
@@ -161,6 +161,10 @@ const UserManagement = () => {
 
     }
 
+    const getCurrentUser = async (user) => {
+        setCurrentUser(user);
+    }
+
     const handleRoleChange = async () => {
         try {
             const data = {
@@ -185,6 +189,27 @@ const UserManagement = () => {
     }
 
     // remove user from team
+    const handleRemoveUser = async () => {
+        try {
+            const data = {
+                teamId: team[0].teamId,
+                userId: currentUser._id
+            }
+            const response = await Api.post('/api/admin/remove/user/team', data, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                }
+            });
+            const data2 = await response.data;
+            setSuccess(data2);
+            togglePopup3();
+            getUsersAndRoles();
+        }
+        catch (error) {
+            setError(error.response.data);
+            togglePopup3();
+        }
+    }
 
     return (
         <div>
@@ -271,6 +296,40 @@ const UserManagement = () => {
 
                     </>}
                 />}
+            {/* popup for remove user from team */}
+            {
+                isOpen3 && <PopUp
+                    content={<>
+                        <div id="staticModal" data-modal-backdrop="static" tabIndex="-1" aria-hidden="true" className="fixed top-0 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                            <div className="relative w-full max-w-2xl max-h-full" style={{ margin: "auto" }}>
+                                <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+
+                                    <div className="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
+                                        <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+                                           Remove <span className='text-green-700'>{currentUser.email}</span> from {team[0]?.teamName}
+                                        </h3>
+                                        <button type="button" onClick={togglePopup2} className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="staticModal">
+                                            <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                                            </svg>
+                                            <span className="sr-only">Close modal</span>
+                                        </button>
+                                    </div>
+                                    <div className="p-6 space-y-6">
+                                        <div>
+                                            <p>Confirm Action ?</p>
+                                        </div>
+                                    </div>
+                                    <div style={{ textAlign: 'center', paddingTop: '1rem' }}>
+                                        <button type="button" onClick={handleRemoveUser} className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Yes</button>
+                                        <button type="button" onClick={togglePopup3} className="text-white bg-red-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">No</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </>}
+                />
+            }
 
             <div>
                 {error && <div className="text-red-500 text-sm p-2 text-center">
@@ -327,7 +386,7 @@ const UserManagement = () => {
                                                 {user.user.email}
                                             </td>
                                             <td className="px-6 py-4">
-                                                <a href="#" className="font-medium text-red-600 hover:underline">Remove User</a>
+                                                <a href="#" onClick={() => { togglePopup3(); getCurrentUser(user.user); }} className="font-medium text-red-600 hover:underline">Remove User</a>
                                             </td>
                                         </tr>
                                     ))
