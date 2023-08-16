c1101ea286f350523487d80e6d31faf02fe1d699
fredcodee <fredcode23@gmail.com>
2023-08-17 00:39:06 +0800

    fixed add/remove permissions

--- client/src/pages/ProjectPageAdmin.jsx
+++ client/src/pages/ProjectPageAdmin.jsx
@@ -258,13 +258,18 @@ const ProjectPageAdmin = () => {
 
     const handleaddPermission = async () => {
         try {
+            if (currentPermission === '') {
+                setError('Please select a permission');
+                togglePopUpForRolesDelete();
+                setCurrentPermission('');
+                return;
+            }
             const data = {
                 teamId: team[0]?.teamId,
                 projectId: id,
-                roleId: currentRole,
+                roleId: currentRole.role._id,
                 permissionId: currentPermission
             }
-            console.log(data)
             const response = await Api.post('/api/admin/permission/add/role', data, {
                 headers: {
                     Authorization: `Bearer ${token}`,
@@ -275,7 +280,7 @@ const ProjectPageAdmin = () => {
             setError(null);
             togglePopUpForRolesAdd();
             rolesAndPermissions();
-            
+
 
 
         } catch (error) {
@@ -284,6 +289,40 @@ const ProjectPageAdmin = () => {
             togglePopUpForRolesAdd();
         }
     }
+
+    const handleRemovePermission = async () => {
+        try {
+            if (currentPermission === '') {
+                setError('Please select a permission');
+                togglePopUpForRolesDelete();
+                setCurrentPermission('');
+                return;
+            }
+            const data = {
+                teamId: team[0]?.teamId,
+                projectId: id,
+                roleId: currentRole.role._id,
+                permissionId: currentPermission
+            }
+
+            const response = await Api.post('/api/admin/permission/remove/role', data, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            });
+            const data2 = await response.data;
+            setSuccess(data2.message);
+            setError(null);
+            togglePopUpForRolesDelete();
+            rolesAndPermissions();
+
+        } catch (error) {
+            setError(error.response?.data?.message || "An error occurred");
+            setSuccess(null); // Clear success message on error
+            togglePopUpForRolesAdd();
+        }
+    }
+
 
 
 
@@ -487,6 +526,46 @@ const ProjectPageAdmin = () => {
             }
 
             {/* popup for remove permission */}
+            {
+                showPopUpForRolesDelete && <PopUp
+                    content={<>
+                        <div id="staticModal" data-modal-backdrop="static" tabIndex="-1" aria-hidden="true" className="fixed top-0 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                            <div className=" relative w-full max-w-2xl max-h-full" style={{ margin: "auto" }}>
+                                <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+
+                                    <div className="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
+                                        <h3 className="text-xl font-semibold dark:text-white">
+                                            Remove permission to this role
+                                        </h3>
+                                        <button type="button" onClick={togglePopUpForRolesDelete} className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="staticModal">
+                                            <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                                            </svg>
+                                            <span className="sr-only">Close modal</span>
+                                        </button>
+                                    </div>
+                                    <div className="p-6 space-y-6">
+                                        <div>
+                                            <select id="role" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" onChange={e => setCurrentPermission(e.target.value)}>
+                                                <option value= "">Select a permission</option>
+                                                {
+                                                    currentRole.permissions.map((permission, index) => {
+                                                        return <option key={index} value={permission._id}>{permission.name}</option>
+                                                    })
+                                                }
+                                            </select>
+                                            <div className='text-center p-4'>
+                                                <button type="button" onClick={handleRemovePermission} className="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">Remove</button>
+
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </>}
+                />
+            }
 
 
 
@@ -604,15 +683,18 @@ const ProjectPageAdmin = () => {
                                         <button
                                             type="button"
                                             onClick={() => {
-                                                setCurrentRole(role.role._id);
+                                                setCurrentRole(role);
                                                 togglePopUpForRolesAdd();
                                             }}
-                                            className="text-white bg-purple-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+                                            className="text-white bg-purple-700 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
                                         >
                                             Add
                                         </button>
 
-                                        <button type="button" className="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">remove</button>
+                                        <button type="button" onClick={() => {
+                                            setCurrentRole(role);
+                                            togglePopUpForRolesDelete();
+                                        }} className="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">remove</button>
                                         <hr />
                                     </div>
                                 )}
